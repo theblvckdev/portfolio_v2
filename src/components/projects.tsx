b7c19a846820261f4f1fffae6085afd737e72594
theblvckdev <nathancodes05@gmail.com>
2024-06-12 15:16:52 +0100

    project section complete ✅

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { IoArrowForwardOutline } from "react-icons/io5";
+import ProjectCard from "./ui/project_card";
 
 interface ToolsTypes {
   name: string;
@@ -11,6 +11,7 @@ interface Project {
   name: string;
   description: string;
   url: string;
+  imageURL: string;
   tools: ToolsTypes[];
   // Add other fields as necessary
 }
@@ -54,73 +55,17 @@ const Projects = () => {
         <div className="mt-10">
           <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5">
             {projects.map((data, index) => {
-              const { name, description, url, tools } = data;
+              const { name, description, url, tools, imageURL } = data;
 
               return (
-                <div key={index}>
-                  <div
-                    data-aos="fade-up"
-                    data-aos-duration="1000"
-                    className="bg-secondary-0 p-3 lg:rounded-3xl rounded-2xl ring-1 ring-gray-800"
-                  >
-                    <div className="h-[200px] bg-primary-0 rounded-xl w-full overflow-hidden"></div>
-                    <div className="mt-3">
-                      <h4 className="text-white font-poppins font-semibold text-xl">
-                        {name}
-                      </h4>
-
-                      <p className="mt-2 text-sm text-gray-400 font-poppins">
-                        {description}
-                      </p>
-
-                      <div className="mt-4">
-                        <div className="flex flex-row items-center">
-                          <div className="mr-auto flex gap-2">
-                            {tools.map((data, index) => {
-                              const { image, name } = data;
-
-                              return (
-                                <div
-                                  key={index}
-                                  className="h-[30px] w-[30px] rounded-full ring-1 ring-gray-500 flex items-center justify-center bg-black"
-                                >
-                                  <img
-                                    src={image}
-                                    className="w-[50%] h-[50%]"
-                                    alt={name}
-                                  />
-                                </div>
-                              );
-                            })}
-                          </div>
-
-                          <div>
-                            <a
-                              href={url}
-                              target="_blank"
-                              className={`no-underline tracking-tighter outline-none text-primary-0 text-sm font-code ${
-                                url === null
-                                  ? "cursor-not-allowed"
-                                  : "cursor-pointer"
-                              }`}
-                            >
-                              {url === "coming soon" ? (
-                                "Coming soon"
-                              ) : (
-                                <>
-                                  <div className="flex items-center gap-2">
-                                    <div>Check Live Site</div>
-                                    <IoArrowForwardOutline />
-                                  </div>
-                                </>
-                              )}
-                            </a>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                <ProjectCard
+                  name={name}
+                  description={description}
+                  imageURL={imageURL}
+                  tools={tools}
+                  url={url}
+                  key={index}
+                />
               );
             })}
           </div>
--- src/components/ui/project_card.tsx
+++ src/components/ui/project_card.tsx
@@ -1,17 +1,89 @@
-import { ReactElement } from "react";
+import { IoArrowForwardOutline } from "react-icons/io5";
 
-interface ComponentPropTypes {
-  additionalClasses?: string;
-  body: ReactElement;
+interface ToolsTypes {
+  name: string;
+  image: string;
 }
 
-const ProjectCard = ({ additionalClasses, body }: ComponentPropTypes) => {
+interface ComponentProp {
+  name: string;
+  description: string;
+  url: string;
+  imageURL: string;
+  tools: ToolsTypes[];
+  // Add other fields as necessary
+}
+
+const ProjectCard = ({
+  imageURL,
+  name,
+  description,
+  tools,
+  url,
+}: ComponentProp) => {
   return (
     <>
       <div
-        className={`ring-1 ring-gray-800 p-5 rounded-2xl ${additionalClasses}`}
+        data-aos="fade-up"
+        data-aos-duration="1000"
+        className="bg-secondary-0 p-3 lg:rounded-3xl rounded-2xl ring-1 ring-gray-800"
       >
-        {body}
+        <div className="h-[180px] rounded-xl w-full overflow-hidden">
+          <img
+            src={imageURL}
+            className="h-full w-full object-cover"
+            alt={name}
+          />
+        </div>
+        <div className="mt-3">
+          <h4 className="text-white font-poppins font-semibold text-xl">
+            {name}
+          </h4>
+
+          <p className="mt-2 text-sm text-gray-400 font-poppins">
+            {description}
+          </p>
+
+          <div className="mt-4">
+            <div className="flex flex-row items-center">
+              <div className="mr-auto flex gap-2">
+                {tools.map((data, index) => {
+                  const { image, name } = data;
+
+                  return (
+                    <div
+                      key={index}
+                      className="h-[30px] w-[30px] rounded-full ring-1 ring-gray-500 flex items-center justify-center bg-black"
+                    >
+                      <img src={image} className="w-[50%] h-[50%]" alt={name} />
+                    </div>
+                  );
+                })}
+              </div>
+
+              <div>
+                <a
+                  href={url}
+                  target="_blank"
+                  className={`no-underline tracking-tighter outline-none text-primary-0 text-sm font-code ${
+                    url === null ? "cursor-not-allowed" : "cursor-pointer"
+                  }`}
+                >
+                  {url === "coming soon" ? (
+                    "Coming soon"
+                  ) : (
+                    <>
+                      <div className="flex items-center gap-2">
+                        <div>Check Live Site</div>
+                        <IoArrowForwardOutline />
+                      </div>
+                    </>
+                  )}
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
