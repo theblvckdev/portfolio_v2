39ed0f8ad4f7eb8b081d603493aaa2be93221eda
theblvckdev <nathancodes05@gmail.com>
2024-09-11 03:33:30 +0100

    added animated cursor

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Reviews from "./components/reviews";
 import Experience from "./components/experience";
 import Contact from "./components/contact";
 import Footer from "./components/footer";
+import AnimatedCursor from "react-animated-cursor";
 
 const App = () => {
   useEffect(() => {
@@ -27,6 +28,32 @@ const App = () => {
       <Experience />
       <Contact />
       <Footer />
+
+      <AnimatedCursor
+        innerSize={8}
+        outerSize={50}
+        color="203,172,249"
+        outerAlpha={0.2}
+        innerScale={0.7}
+        outerStyle={{
+          backgroundColor: "transparent",
+          border: "2px solid rgb(203,172,249)",
+        }}
+        outerScale={2}
+        clickables={[
+          "a",
+          'input[type="text"]',
+          'input[type="email"]',
+          'input[type="number"]',
+          'input[type="submit"]',
+          'input[type="image"]',
+          "label[for]",
+          "select",
+          "textarea",
+          "button",
+          ".link",
+        ]}
+      />
     </>
   );
 };
